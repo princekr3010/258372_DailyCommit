7f6f25b8e7da7706ce03c96b0ea5fc63acd6b6df
princekr3010 <68175614+princekr3010@users.noreply.github.com>
2021-04-22 13:53:23 +0530

    Adding Extra File in Github

--- main.py
+++ main.py
@@ -1,150 +1,19 @@
-def Cal(a, b):
-    print(a+b)
+def fun():
+    list = [1,2,3,4,5]
+    print(list[:-1])
+    print(list[2:5])
+    print(list[3:-1])
+    l = list[1:3]
+    l.append(8)
+    l.insert(1,9)
+    l.remove(2)
+    l.reverse()
+    print(l)
 
 
-Cal(10,25.7)
-
-
-a = input("enter a number:")
-b = input("enter a number:")
-res = a + b
-print(res)
-print(type(res))
-print(type(a))
-print(type(b))
-
-hh, mm, ss = 10, 12, 25
-print(hh)
-
-x=10
-y=2.3
-s1="hello"
-ch='A'
-flag=True
-c1=2+3j
-print(type(c1))
-print(isinstance(x, int))
-
-
-
-
-fval=2.3
-ival=int(fval)
-print(ival)
-
-ch='A'
-aval=ord(ch)
-print(aval)
-
-s1="2378"
-ival=int(s1)
-s2="45a64"
-
-ival=23
-s3=str(ival)
-print(s3)
-
-hval="23"
-ival=int(hval,16)
-print(ival)
-
-s4="12.235"
-fval=float(s4)
-
-print(int(0x1D))
-print(hex(45))
-print(oct(0o23))
-print(bin(13))
-print(int(0b11011))
-
-
-
-a=32
-b=10
-c=a/b
-d=a//b
-print(c,d)
-x=2
-y=5
-z=x**y
-q=x(int(y)-5)
-x+=5
-
-
-
-a=10
-b=2
-c=0
-print(a and b)
-print(b and a)
-print(a and c)
-print(c and b)
-print(a or b)
-print(b or a)
-print(a or c)
-print(c or b)
-
-
-
-s1="Hello"
-s2="Hello"
-ch='e'
-c2='x'
-print(s1==s2)
-print(s1 is s2)
-print(ch in s1)
-print(c2 not in s2)
-
-
-
-
-xval = -12.785
-print(abs(xval))
-print(round(xval,2))
-
-
-s1="abcd"
-s2='xyz'
-print(s1+s2)
-
-
-
-s1="10"
-s2="20"
-s3=int(s1)+int(s2)
-print(s3)
-
-
-s1="abcd"
-s1*=3
-print(s1)
-s1+="xyz"
-print(s1)
-print("hello " * 3)
-print(4 * "hello ")
-
-
-
-
-a,b=10,20
-a,b=b,a
-print(a,b)
-
-
-
-a=10
-b=20
-a=a^b
-b=a^b
-a=a^b
-print(a,b)
-
-
-
-n=2378
-a=(n/100)%10
-b=(n/10)%10
-c=(n%100)/10
-d=(n%1000)/100
-
-
+    list_2d = [[1,2,3],[4,5,6,7],[8,9]]
+    print(list_2d)
+    print(list_2d[1][3])
+    list_2d.extend([[2,3,4,5]])
+    print(list_2d)
+fun()
