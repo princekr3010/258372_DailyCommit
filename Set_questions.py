2b0c74bbf098376dc54bada259c32974944d4d1a
princekr3010 <68175614+princekr3010@users.noreply.github.com>
2021-04-23 11:30:42 +0530

    Adding Extra File in Github

---
+++ Set_questions.py
@@ -0,0 +1,9 @@
+# Write a program to find maximum and the minimum value in set
+
+def min_max():
+    set_a = set([5, 10, 15, 20, 25, 2, 4, 6])
+    print(max(set_a))
+    print(min(set_a))
+
+
+min_max()
